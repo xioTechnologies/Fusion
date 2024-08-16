a093f6d918ebc6d0abbdee7dfc4ca69779b1caa3
x-io Technologies <social@x-io.co.uk>
2024-08-16 11:33:46 +0100

    Modernise Python build

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 import numpy
-import os
-import shutil
 import sys
 from setuptools import setup, Extension
-
-for folder in ["build", "dist", "imufusion.egg-info"]:
-    if os.path.exists(folder) and os.path.isdir(folder):
-        shutil.rmtree(folder)
-
-if len(sys.argv) == 1:  # if this script was called without arguments
-    sys.argv.append("install")
-    sys.argv.append("--user")
-
-github_url = "https://github.com/xioTechnologies/Fusion"
 
 ext_modules = Extension("imufusion", ["Python/Python-C-API/imufusion.c",
                                       "Fusion/FusionAhrs.c",
@@ -22,16 +10,4 @@ ext_modules = Extension("imufusion", ["Python/Python-C-API/imufusion.c",
                         define_macros=[("FUSION_USE_NORMAL_SQRT", None)],
                         libraries=(["m"] if "linux" in sys.platform else []))  # link math library for Linux
 
-setup(name="imufusion",
-      version="1.2.5",
-      description="Fusion Python package",
-      long_description="See [github](" + github_url + ") for documentation and examples.",
-      long_description_content_type='text/markdown',
-      url=github_url,
-      author="x-io Technologies Limited",
-      author_email="info@x-io.co.uk",
-      license="MIT",
-      classifiers=["Programming Language :: Python :: 3.10",
-                   "Programming Language :: Python :: 3.11",
-                   "Programming Language :: Python :: 3.12"],  # versions shown by pyversions badge in README
-      ext_modules=[ext_modules])
+setup(ext_modules=[ext_modules])
