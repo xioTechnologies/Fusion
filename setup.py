9037d604c7259331846d41b49c2cae7b3f10e3f6
x-io Technologies <social@x-io.co.uk>
2025-12-09 15:17:55 +0000

    Add Python stub file

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-import numpy
 import sys
-from setuptools import setup, Extension
 
-ext_modules = Extension("imufusion", ["Python/imufusion/imufusion.c",
-                                      "Fusion/FusionAhrs.c",
-                                      "Fusion/FusionCompass.c",
-                                      "Fusion/FusionOffset.c"],
-                        include_dirs=[numpy.get_include()],
-                        define_macros=[("FUSION_USE_NORMAL_SQRT", None)],
-                        libraries=(["m"] if "linux" in sys.platform else []))  # link math library for Linux
+import numpy
+from setuptools import Extension, setup
 
-setup(ext_modules=[ext_modules])
+ext_modules = Extension(
+    "imufusion",
+    ["Python/imufusion/imufusion.c", "Fusion/FusionAhrs.c", "Fusion/FusionCompass.c", "Fusion/FusionOffset.c"],
+    include_dirs=[numpy.get_include()],
+    define_macros=[("FUSION_USE_NORMAL_SQRT", None)],
+    libraries=(["m"] if "linux" in sys.platform else []),
+)  # link math library for Linux
+
+setup(
+    ext_modules=[ext_modules],
+    packages=["imufusion-stubs"],
+    package_dir={"imufusion-stubs": "Python/imufusion-stubs"},
+)
