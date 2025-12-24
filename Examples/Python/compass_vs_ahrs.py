e2037b559bace154d4399e5accc219b51cb0fe32
x-io Technologies <social@x-io.co.uk>
2025-12-24 12:36:32 +0000

    Refactor examples

--- Examples/Python/compass_vs_ahrs.py
+++ Examples/Python/compass_vs_ahrs.py
@@ -1,29 +1,34 @@
+import os
 import sys
 
 import imufusion
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Import sensor data
-data = np.genfromtxt("sensor_data.csv", delimiter=",", skip_header=1)
+# Read sensor data CSV
+examples_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-sample_rate = 100  # 100 Hz
+file_path = os.path.join(examples_directory, "Sensor Data.csv")
 
-seconds = data[:, 0]
-gyroscope = data[:, 1:4]
-accelerometer = data[:, 4:7]
-magnetometer = data[:, 7:10]
+sensor_data = np.genfromtxt(file_path, delimiter=",", skip_header=1)
+
+seconds = sensor_data[:, 0]
+gyroscope = sensor_data[:, 1:4]
+accelerometer = sensor_data[:, 4:7]
+magnetometer = sensor_data[:, 7:10]
 
 # Compass heading
 compass_heading = [imufusion.compass(a, m) for a, m in zip(accelerometer, magnetometer)]
 
 # AHRS heading
+sample_rate = 100  # Hz
+
 ahrs = imufusion.Ahrs()
 
 ahrs.settings = imufusion.AhrsSettings(
     sample_rate,
-    magnetic_rejection=10,  # reject magnetic disturbances >10 degrees
-    recovery_trigger_period=20 * sample_rate,  # reject magnetic disturbances for up to 20 seconds
+    magnetic_rejection=10,  # reject magnetic errors >10 degrees
+    recovery_trigger_period=int(20 * sample_rate),  # reject magnetic disturbances for up to 20 seconds
 )
 
 ahrs_heading = np.empty_like(seconds)
@@ -34,9 +39,9 @@ for index, _ in enumerate(seconds):
     ahrs_heading[index] = imufusion.quaternion_to_euler(ahrs.quaternion)[2]
 
 # Plot
-plt.title("Heading")
 plt.plot(seconds, compass_heading, label="Compass")
 plt.plot(seconds, ahrs_heading, label="AHRS")
+plt.title("Heading")
 plt.xlabel("Seconds")
 plt.ylabel("Degrees")
 plt.legend()
