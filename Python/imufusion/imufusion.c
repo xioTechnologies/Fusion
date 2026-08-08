ba73ec64ab825e1cead0ccf8a7efec5a13c35736
x-io Technologies <social@x-io.co.uk>
2026-08-08 05:20:54 +0100

    Fix inconsistent enum order

--- Python/imufusion/imufusion.c
+++ Python/imufusion/imufusion.c
@@ -39,29 +39,29 @@ PyMODINIT_FUNC PyInit_imufusion() {
 
     if (module != NULL &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PXPYPZ", FusionRemapAlignmentPXPYPZ) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_PXPZNY", FusionRemapAlignmentPXPZNY) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PXNZPY", FusionRemapAlignmentPXNZPY) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PXNYNZ", FusionRemapAlignmentPXNYNZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_PXPZNY", FusionRemapAlignmentPXPZNY) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NXPYNZ", FusionRemapAlignmentNXPYNZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NXPZPY", FusionRemapAlignmentNXPZPY) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NXNYPZ", FusionRemapAlignmentNXNYPZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NXNZNY", FusionRemapAlignmentNXNZNY) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_PYNXPZ", FusionRemapAlignmentPYNXPZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_PYNZNX", FusionRemapAlignmentPYNZNX) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PYPXNZ", FusionRemapAlignmentPYPXNZ) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PYPZPX", FusionRemapAlignmentPYPZPX) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NYPXPZ", FusionRemapAlignmentNYPXPZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NYNZPX", FusionRemapAlignmentNYNZPX) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NYNXNZ", FusionRemapAlignmentNYNXNZ) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NYPZNX", FusionRemapAlignmentNYPZNX) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_PZPYNX", FusionRemapAlignmentPZPYNX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_PYNZNX", FusionRemapAlignmentPYNZNX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_PYNXPZ", FusionRemapAlignmentPYNXPZ) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PZPXPY", FusionRemapAlignmentPZPXPY) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_PZPYNX", FusionRemapAlignmentPZPYNX) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PZNYPX", FusionRemapAlignmentPZNYPX) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_PZNXNY", FusionRemapAlignmentPZNXNY) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NZPYPX", FusionRemapAlignmentNZPYPX) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NZNXPY", FusionRemapAlignmentNZNXPY) == 0) &&
-        (PyModule_AddIntConstant(module, "ALIGNMENT_NZNYNX", FusionRemapAlignmentNZNYNX) == 0) &&
         (PyModule_AddIntConstant(module, "ALIGNMENT_NZPXNY", FusionRemapAlignmentNZPXNY) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NZPYPX", FusionRemapAlignmentNZPYPX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NZNYNX", FusionRemapAlignmentNZNYNX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NZNXPY", FusionRemapAlignmentNZNXPY) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NYPXPZ", FusionRemapAlignmentNYPXPZ) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NYPZNX", FusionRemapAlignmentNYPZNX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NYNZPX", FusionRemapAlignmentNYNZPX) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NYNXNZ", FusionRemapAlignmentNYNXNZ) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NXPYNZ", FusionRemapAlignmentNXPYNZ) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NXPZPY", FusionRemapAlignmentNXPZPY) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NXNZNY", FusionRemapAlignmentNXNZNY) == 0) &&
+        (PyModule_AddIntConstant(module, "ALIGNMENT_NXNYPZ", FusionRemapAlignmentNXNYPZ) == 0) &&
         (PyModule_AddIntConstant(module, "CONVENTION_NWU", FusionConventionNwu) == 0) &&
         (PyModule_AddIntConstant(module, "CONVENTION_ENU", FusionConventionEnu) == 0) &&
         (PyModule_AddIntConstant(module, "CONVENTION_NED", FusionConventionNed) == 0) &&
